fe9c7a7c60158996d414113d936d30780bef6ee0
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-04-30 15:01:10 +0200

    ADD - Added 2 redirections methods

--- backendninja/src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.udemy.backendninja.model.Person;
 
@@ -17,6 +18,20 @@ public class Example3Controller {
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
 
+	//REDIRECTION
+	
+	//WAY ONE
+//	@GetMapping("/")
+//	public String redirect() {
+//		return "redirect:/example3/showForm";
+//	}
+	
+	//WAY TWO (recommended by Spring)
+	@GetMapping("/")
+	public RedirectView redirect() {
+		return new RedirectView("/example3/showForm");
+	}
+	
 	@GetMapping("/showForm")
 	public String showForm(Model model) {
 		model.addAttribute("person", new Person());
