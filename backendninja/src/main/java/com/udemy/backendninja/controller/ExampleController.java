bd8730d67aa5eb806aaec08c3f0ff4265fa17822
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-04-30 19:31:42 +0200

    ADD - Added a new Component with a log injected in a controller

--- backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -3,12 +3,15 @@ package com.udemy.backendninja.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.backendninja.component.ExampleComponent;
 import com.udemy.backendninja.model.Person;
 
 @Controller
@@ -17,10 +20,18 @@ public class ExampleController {
 
 	public static final String EXAMPLE_VIEW = "example";
 	
+	@Autowired
+	@Qualifier("exampleComponent")
+	private ExampleComponent exampleComponent;
+	
+	
 	// First way
 	@GetMapping("/exampleString")
 	//@RequestMapping(value = "/exampleString", method = RequestMethod.GET)
 	public String exampleString(Model model) {
+		
+		exampleComponent.sayHello();
+		
 		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
