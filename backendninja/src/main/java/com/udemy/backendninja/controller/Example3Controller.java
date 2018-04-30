e67e3a3816244fbf6c40cbdb69a7963fee59259b
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-04-30 19:15:30 +0200

    ADD - Added log to the controller

--- backendninja/src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -1,5 +1,7 @@
 package com.udemy.backendninja.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,35 +17,46 @@ import com.udemy.backendninja.model.Person;
 @RequestMapping("/example3")
 public class Example3Controller {
 
+	private static final Log LOGGER = LogFactory.getLog(Example3Controller.class);
+
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
 
-	//REDIRECTION
-	
-	//WAY ONE
-//	@GetMapping("/")
-//	public String redirect() {
-//		return "redirect:/example3/showForm";
-//	}
-	
-	//WAY TWO (recommended by Spring)
+	// REDIRECTION
+
+	// WAY ONE
+	// @GetMapping("/")
+	// public String redirect() {
+	// return "redirect:/example3/showForm";
+	// }
+
+	// WAY TWO (recommended by Spring)
 	@GetMapping("/")
 	public RedirectView redirect() {
 		return new RedirectView("/example3/showForm");
 	}
-	
+
 	@GetMapping("/showForm")
 	public String showForm(Model model) {
+
+		LOGGER.info("INFO TRACE");
+		LOGGER.warn("WARN TRACE");
+		LOGGER.error("ERROR TRACE");
+		LOGGER.debug("DEBUG TRACE");
+
 		model.addAttribute("person", new Person());
-		int i = 5 / 0;
 		return FORM_VIEW;
 	}
-	
+
 	@PostMapping("/addPerson")
 	public ModelAndView addPerson(@ModelAttribute("person") Person person) {
 
+		LOGGER.info("METHOD: 'addPerson' -- PARAMS: '" + person + "'");
+
 		ModelAndView mav = new ModelAndView(RESULT_VIEW);
 		mav.addObject("person", person);
+
+		LOGGER.info("TEMPLATE: '" + RESULT_VIEW + "' -- DATA: '" + person + "'");
 
 		return mav;
 	}
