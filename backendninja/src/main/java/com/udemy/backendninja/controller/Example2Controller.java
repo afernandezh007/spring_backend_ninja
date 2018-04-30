561b67d0bd6690970b77a0a3c826bad4cf0983fb
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-04-30 14:30:26 +0200

    UPD - Updated controller to use PathVariable and pass params without "?param=XX"

--- backendninja/src/main/java/com/udemy/backendninja/controller/Example2Controller.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/Example2Controller.java
@@ -2,6 +2,7 @@ package com.udemy.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +19,32 @@ public class Example2Controller {
 		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
 		mav.addObject("nm_in_model", name);
 
+		return mav;
+	}
+	
+	/**
+	 * Using PathVariable the default value doesn't exist. The only way to solve this it's overloading the method
+	 * without params to manage this situation
+	 * @param name
+	 * @return
+	 */
+	@GetMapping("/request2/{nm}")
+	public ModelAndView request2(@PathVariable(name = "nm") String name) {
+
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+		mav.addObject("nm_in_model", name);
+
+		return mav;
+	}
+	
+	/**
+	 * Overloaded version to manage a request without params
+	 * @return
+	 */
+	@GetMapping("/request2")
+	public ModelAndView request2() {
+
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
 		return mav;
 	}
 }
