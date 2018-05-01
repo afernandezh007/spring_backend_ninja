398daa4db900b139a2fbf68fee42993125c66896
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-05-01 19:38:28 +0200

    UPD - Updated contact controller to add the cancel and the addContact requests

--- backendninja/src/main/java/com/udemy/backendninja/controller/ContactController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ContactController.java
@@ -1,17 +1,42 @@
 package com.udemy.backendninja.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.udemy.backendninja.constant.ViewConstant;
+import com.udemy.backendninja.model.ContactModel;
 
 @Controller
 @RequestMapping("/contacts")
 public class ContactController {
 
+	private static final Log LOGGER = LogFactory.getLog(ContactController.class);
+
+	@GetMapping("/cancel")
+	public String cancel() {
+		return ViewConstant.CONTACTS;
+	}
+
 	@GetMapping("/contactForm")
-	public String redirectContactForm() {
+	public String redirectContactForm(Model model) {
+
+		model.addAttribute("contactModel", new ContactModel());
 		return ViewConstant.CONTACT_FORM;
+	}
+
+	@PostMapping("/addContact")
+	public String addContacts(Model model, @ModelAttribute(name = "contactModel") ContactModel contactModel) {
+
+		LOGGER.info("Method: addContact() -- PARAMS: contactModel=" + contactModel);
+
+		model.addAttribute("result", 1);
+
+		return ViewConstant.CONTACTS;
 	}
 }
