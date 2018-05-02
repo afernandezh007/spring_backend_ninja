f5765fc86b13b6f94e036db9e90678493fb0f8f2
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-05-02 14:29:54 +0200

    ADD - Added Log entity to save the log of the application

--- backendninja/src/main/java/com/udemy/backendninja/component/RequestTimeInterceptor.java
+++ backendninja/src/main/java/com/udemy/backendninja/component/RequestTimeInterceptor.java
@@ -1,17 +1,29 @@
 package com.udemy.backendninja.component;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.udemy.backendninja.repository.LogRepository;
 
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
 
 	private static final Log LOGGER = LogFactory.getLog(RequestTimeInterceptor.class);
+
+	@Autowired
+	@Qualifier("logRepository")
+	private LogRepository logRepository;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -24,8 +36,20 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		long startTime = (long) request.getAttribute("startTime");
-		LOGGER.info("--Url to: '" + request.getRequestURL().toString() + "' in: "
-				+ (System.currentTimeMillis() - startTime) + " ms");
+
+		String url = request.getRequestURL().toString();
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		String userName = "";
+		if (auth != null && auth.isAuthenticated()) {
+			userName = auth.getName();
+		}
+
+		logRepository
+				.save(new com.udemy.backendninja.entity.Log(new Date(), auth.getDetails().toString(), userName, url));
+
+		LOGGER.info("--Url to: '" + url + "' in: " + (System.currentTimeMillis() - startTime) + " ms");
 	}
 
 }
