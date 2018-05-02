da90ed346ac6a61b3b9ad6be4018de1517c0e9ff
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2018-05-02 14:13:03 +0200

    ADD - Added example of authorization at method or class level

--- backendninja/src/main/java/com/udemy/backendninja/controller/ContactController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ContactController.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +33,7 @@ public class ContactController {
 		return "redirect:/contacts/showContacts";
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER')") //this annotation can be in method or in class even in services
 	@GetMapping("/contactForm")
 	public String redirectContactForm(@RequestParam(name="id", required = false) int id, Model model) {
 
